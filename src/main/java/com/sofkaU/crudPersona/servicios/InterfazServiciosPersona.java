10a135e4bdb21347ad0ce5e3e6448b63f0741b96
NatalyMora21 <tatamora21gmail.com>
2021-09-11 22:29:03 -0500

    CActualizado mÃétodo listId que llama al método findById del repositorio

--- src/main/java/com/sofkaU/crudPersona/servicios/InterfazServiciosPersona.java
+++ src/main/java/com/sofkaU/crudPersona/servicios/InterfazServiciosPersona.java
@@ -2,11 +2,12 @@ package com.sofkaU.crudPersona.servicios;
 import com.sofkaU.crudPersona.entidades.Persona;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InterfazServiciosPersona {
 
     public List<Persona> listar();
-    public Persona listarId(int id);
+    public Optional<Persona> listarId(int id);
     public Persona guardar(Persona persona);
     public void borar(int id);
     public Persona actualizar(Persona persona);
